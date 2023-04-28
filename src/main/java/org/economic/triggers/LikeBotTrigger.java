c0132437ac4a4ae4dd2b4ac3ebe2790256a95328
timka <timkaa5@yandex.ru>
2023-04-28 20:23:01 +0500

    Trigger for like bot added

---
+++ src/main/java/org/economic/triggers/LikeBotTrigger.java
@@ -0,0 +1,37 @@
+package org.economic.triggers;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.economic.database.user.User;
+import org.economic.database.user.UserDAOImplement;
+
+public class LikeBotTrigger {
+    UserDAOImplement userDAOImplement = new UserDAOImplement();
+    final public String botID = "575776004233232386";
+
+    public void trigger(MessageReceivedEvent event){
+       if(event.getMessage().getEmbeds().get(0).getDescription().contains("Вы успешно лайкнули")){
+          String st = event.getMessage().getEmbeds().get(0).getAuthor().getName();
+          String[] name = st.split("#");
+
+          Member member = event.getGuild().getMemberByTag(name[0], name[1]);
+           User user = userDAOImplement.findByID(member.getIdLong());
+           if (user == null) {
+               userDAOImplement.addUser(new User(member.getIdLong(), 300));
+           }
+           else userDAOImplement.setBalance(user, +300);
+
+           event.getGuild()
+                   .getTextChannelById(1092836254514696222L)
+                   .sendMessage(member.getEffectiveName() + "Лайкнул сервер")
+                   .queue();
+       }
+
+    }
+    public boolean isFromBot(MessageReceivedEvent event){
+        return event.getMessage().getAuthor().isBot() && event.getAuthor().getId().equals(botID);
+
+    }
+
+
+}
